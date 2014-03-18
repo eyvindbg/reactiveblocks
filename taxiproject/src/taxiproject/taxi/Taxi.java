b889ea502a6fe01750ccb96140a67d0d2a0829a8
eyvindbg <eyvindbg@gmail.com>
2014-03-18 20:01:29 +0100

    Kjører, men MQTT funker ikke

--- taxiproject/src/taxiproject/taxi/Taxi.java
+++ taxiproject/src/taxiproject/taxi/Taxi.java
@@ -4,9 +4,14 @@ import no.ntnu.item.arctis.runtime.Block;
 
 public class Taxi extends Block {
 
-	public java.lang.String subscription = "order";
+	public java.lang.String subscription;
+	
+	public Taxi () {
+		this.subscription = "order";
+	}
 
 	public String echoTest() {
+		System.out.println("Subscription!!!");
 		return "Test successful";
 	}
 
--- taxiproject/src/taxiproject/user/User.java
+++ taxiproject/src/taxiproject/user/User.java
@@ -5,7 +5,10 @@ import no.ntnu.item.arctis.runtime.Block;
 public class User extends Block {
 
 	public java.lang.String subscriptionTopic;
-	public java.lang.String publishTopic;
+	
+	public User() {
+		this.subscriptionTopic = "taxi,dispatch";
+	}
 	
 	public String getOrderTopic(Order order) {
 		return order.topic;
