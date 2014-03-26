169772601e2bad99c1a017596a3f15c8aebaa731
Lars Nedberg <larsnedb@gmail.com>
2014-03-26 11:25:27 +0100

    Laget en grov TODO-liste under koden til systemblokken. Kan aksesseres ved å trykke på variabelen "g" som ligger på systemnivået

--- taxiproject/src/taxiproject/system/component/Component.java
+++ taxiproject/src/taxiproject/system/component/Component.java
@@ -4,4 +4,21 @@ import no.ntnu.item.arctis.runtime.Block;
 
 public class Component extends Block {
 
+	public java.lang.String g;
+
+
+	/*
+	//TODO:
+	
+	Ordne cancel order. Dette krever at ordren slettes og sier fra til de den skal (til dispatch, til assignet drosje, og bekreftelse på slettet order
+	
+	Ordne confirm order fra taxiclient-siden. Etter én taxi har godtatt en ordre, skal ikke andre taxier lenger kunne confirme.
+	
+	Det er kun siste objekt som er aktivt for taxiclientene. Dersom UserMSID0 legger inn en order, 1, rett etter UserMSID1 har lagt inn order 0, 
+	uten at noen har rukket å svare på order 0, vil det ikke lenger gå an å aksessere ordren
+	
+	
+	*/
+	
+	
 }
